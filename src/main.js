9fb92471db059f34d6d297693f3078db835af765
Gergely Kovacs <gergely.kovacs@goodgrind.tech>
2020-01-08 14:45:24 +0100

    ran standard on all files

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import './global.css';
-import App from './components/App.svelte';
+import './global.css'
+import App from './components/App.svelte'
 
 const app = new App({
-	target: document.body
-});
+  target: document.body
+})
 
-window.app = app;
+window.app = app
 
-export default app;
+export default app
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,63 @@
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
-const mode = process.env.NODE_ENV || 'development';
-const isProd = mode === 'production';
+const mode = process.env.NODE_ENV || 'development'
+const isProd = mode === 'production'
 
 module.exports = {
-	mode: mode,
-	devtool: isProd ? false : 'source-map',
-	entry: {
-		bundle: ['./src/main.js']
-	},
-	resolve: {
-		alias: {
-			svelte: path.resolve('node_modules', 'svelte')
-		},
-		extensions: ['.mjs', '.js', '.svelte'],
-		mainFields: ['svelte', 'browser', 'module', 'main']
-	},
-	output: {
-		path: __dirname + '/dist',
-		filename: '[name].js',
-		chunkFilename: '[name].[id].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svelte$/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						emitCss: true,
-						hotReload: true
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							hmr: !isProd,
-						},
-					},
-					'css-loader'
-				]
-			}
-		]
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		}),
-		new HtmlWebpackPlugin({
-			title: `Geko's page`,
-			favicon: 'assets/favicon.png'
-		}),
-	]
-};
+  mode: mode,
+  devtool: isProd ? false : 'source-map',
+  entry: {
+    bundle: ['./src/main.js']
+  },
+  resolve: {
+    alias: {
+      svelte: path.resolve('node_modules', 'svelte')
+    },
+    extensions: ['.mjs', '.js', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].js',
+    chunkFilename: '[name].[id].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            emitCss: true,
+            hotReload: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: !isProd
+            }
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Geko\'s page',
+      favicon: 'assets/favicon.png'
+    })
+  ]
+}
