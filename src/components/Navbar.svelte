a06030565a4d00a94b04002394c31c8f526fb32e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-08 14:25:58 +0000

    Deploy gergely-kovacs/bio to github.com/gergely-kovacs/bio.git:gh-pages

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<style>
+	.navbar {
+		display: flex;
+		justify-content: space-around;
+	}
+</style>
+
+<div class="navbar">
+	<div class="navitem">Intro</div>
+	<div class="navitem">Movies</div>
+	<div class="navitem">Video games</div>
+	<div class="navitem">Hiking</div>
+	<div class="navitem">Climbing</div>
+</div>
