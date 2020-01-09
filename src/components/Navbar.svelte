c33ff1d5ccc2cfc34329a043a6b21bc8ec5d4cd5
Gergely Kovacs <gergely-kovacs@hotmail.com>
2020-01-09 23:49:09 +0100

    added dark mode toggler to nav bar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,18 +1,76 @@
 <script>
+  import { onMount } from "svelte";
   import active from "svelte-spa-router/active";
+
+  const DARK_THEME_CLASSES = ["bg-dark", "text-light"];
+  const LIGHT_THEME_CLASSES = ["bg-light", "text-dark"];
+
+  onMount(() =>
+    toggleTheme({
+      target: {
+        checked: true
+      }
+    })
+  );
+
+  function toggleTheme(event) {
+    const isDarkThemeEnabled = event.target.checked;
+    const classesToAdd = isDarkThemeEnabled
+      ? DARK_THEME_CLASSES
+      : LIGHT_THEME_CLASSES;
+    const classesToRemove = !isDarkThemeEnabled
+      ? DARK_THEME_CLASSES
+      : LIGHT_THEME_CLASSES;
+    document.querySelector("body").classList.add(...classesToAdd);
+    document.querySelector("body").classList.remove(...classesToRemove);
+  }
 </script>
 
 <style>
+  .switch {
+    width: 3.5em;
+    height: 2em;
+    border-radius: 2em;
+    background-color: #777;
+  }
 
+  .slider {
+    width: 1.5em;
+    height: 1.5em;
+    border-radius: 1.5em;
+    margin-left: 0.25em;
+    transition: 0.25s;
+    cursor: pointer;
+    background-color: #2196f3;
+  }
+
+  input:checked + .slider {
+    transform: translateX(1.5em);
+    background-color: #222;
+  }
 </style>
 
-<div class="navbar">
-  <nav class="nav nav-pills">
-    <a class="nav-link" use:active href="#/">Intro</a>
-    <a class="nav-link" use:active href="#/movies">Movies</a>
-    <a class="nav-link" use:active href="#/music">Music</a>
-    <a class="nav-link" use:active href="#/gaming">Gaming</a>
-    <a class="nav-link" use:active href="#/hiking">Hiking</a>
-    <a class="nav-link" use:active href="#/climbing">Climbing</a>
+<div class="navbar px-0 pt-0">
+  <nav class="nav nav-pills w-75">
+    <a class="nav-link col text-center rounded-0" use:active href="#/">Intro</a>
+    <a class="nav-link col text-center rounded-0" use:active href="#/movies">
+      Movies
+    </a>
+    <a class="nav-link col text-center rounded-0" use:active href="#/music">
+      Music
+    </a>
+    <a class="nav-link col text-center rounded-0" use:active href="#/gaming">
+      Gaming
+    </a>
+    <a class="nav-link col text-center rounded-0" use:active href="#/hiking">
+      Hiking
+    </a>
+    <a class="nav-link col text-center rounded-0" use:active href="#/climbing">
+      Climbing
+    </a>
   </nav>
+  <label class="mb-0 switch d-flex align-items-center">
+    <input type="checkbox" class="d-none" on:change={toggleTheme} checked />
+    <span class="slider" />
+  </label>
 </div>
